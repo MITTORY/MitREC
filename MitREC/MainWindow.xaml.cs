ec4ce4bb3427cd97d5272a2fd540f29fdf60d8b8
MITTORY <novo1233@mail.ru>
2023-09-30 02:08:49 +0500

    Первоначальный коммит

--- MitREC/MainWindow.xaml.cs
+++ MitREC/MainWindow.xaml.cs
@@ -1,14 +1,31 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.IO;
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Forms;
 using System.Windows.Input;
-using MessageBox = System.Windows.MessageBox;
+using NAudio.Wave;
+using Timer = System.Windows.Threading.DispatcherTimer;
+using Accord.Video;
+using Accord.Video.DirectShow;
+using Accord.Video.FFMPEG;
+using System.Windows.Controls;
+using System.Drawing;
 
 namespace MitREC
 {
     public partial class MainWindow : Window
     {
-        private string savePath = string.Empty;
+        int bitrate = 0;
+        private string savePath;
+        private VideoCaptureDevice videoSource;
+        private ScreenCaptureStream screenCapture;
+        private VideoFileWriter videoWriter;
+        private WaveInEvent waveIn;
+        private WaveFileWriter writer;
+        private Timer timer;
+        private TimeSpan recordingTime;
+        private bool isRecording;
 
         public MainWindow()
         {
@@ -17,12 +34,139 @@ namespace MitREC
 
         private void RecButton_Click(object sender, RoutedEventArgs e)
         {
+            if (waveIn == null)
+            {
+                waveIn = new WaveInEvent();
+                waveIn.DataAvailable += WaveIn_DataAvailable;
 
+                savePath = SaveRoad.Text;
+                if (!string.IsNullOrEmpty(savePath))
+                {
+                    if (audioBox.SelectedItem != null)
+                    {
+                        string selectedQuality = (audioBox.SelectedItem as ComboBoxItem).Content.ToString();
+                        switch (selectedQuality)
+                        {
+                            case ".wav":
+                                // Создаем файл .wav для записи аудио
+                                string fileNameAudioWav = "audio_" + DateTime.Now.ToString("yyyyMMdd_HHmmss") + ".wav";
+                                writer = new WaveFileWriter(Path.Combine(savePath, fileNameAudioWav), waveIn.WaveFormat);
+                                break;
+                            case ".mp3":
+                                // Создаем файл .mp3 для записи аудио
+                                string fileNameAudioMp3 = "audio_" + DateTime.Now.ToString("yyyyMMdd_HHmmss") + ".mp3";
+                                writer = new WaveFileWriter(Path.Combine(savePath, fileNameAudioMp3), waveIn.WaveFormat);
+                                break;
+                        }
+                    }
+
+                    TimeLable.Visibility = Visibility.Visible;
+                    durationTextBlock.Visibility = Visibility.Visible;
+
+                    // Начать запись
+                    waveIn.StartRecording();
+
+                    // Начать отслеживание времени записи
+                    recordingTime = TimeSpan.Zero;
+                    timer = new Timer();
+                    timer.Interval = TimeSpan.FromSeconds(1);
+                    timer.Tick += Timer_Tick;
+                    timer.Start();
+
+                    int width = 1920; // Ширина видео
+                    int height = 1080; // Высота видео
+                    int fps = 30; // Кадры в секунду (FPS)
+                    if (videoQualityBox.SelectedItem != null)
+                    {
+                        string selectedQuality = (videoQualityBox.SelectedItem as ComboBoxItem).Content.ToString();
+                        switch (selectedQuality)
+                        {
+                            case "360p":
+                                bitrate = 5000000;
+                                break;
+                            case "720p":
+                                bitrate = 10000000;
+                                break;
+                            case "1080p":
+                                bitrate = 20000000;
+                                break;
+                        }
+                    }
+
+                    // Запускаем захват рабочего стола
+                    screenCapture = new ScreenCaptureStream(Screen.PrimaryScreen.Bounds, fps);
+                    screenCapture.NewFrame += ScreenCapture_NewFrame;
+                    screenCapture.Start();
+
+                    // Начинаем запись видео
+                    videoWriter = new VideoFileWriter();
+                    string fileName = "video_" + DateTime.Now.ToString("yyyyMMdd_HHmmss") + ".mp4";
+                    videoWriter.Open(Path.Combine(savePath, fileName), width, height, fps, VideoCodec.MPEG4, bitrate);
+
+                    isRecording = true;
+                }
+                else
+                {
+                    System.Windows.MessageBox.Show("Выберите путь для сохранения!");
+                }
+            }
+        }
+
+        private void ScreenCapture_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            if (isRecording && videoWriter != null && videoWriter.IsOpen)
+            {
+                Bitmap frame = eventArgs.Frame;
+                videoWriter.WriteVideoFrame(frame);
+            }
+        }
+
+        private void WaveIn_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            if (writer != null)
+            {
+                writer.Write(e.Buffer, 0, e.BytesRecorded);
+            }
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            recordingTime = recordingTime.Add(TimeSpan.FromSeconds(1));
+            durationTextBlock.Text = recordingTime.ToString(@"mm\:ss");
         }
 
         private void StopButton_Click(object sender, RoutedEventArgs e)
         {
+            if (waveIn != null)
+            {
+                // Остановить запись и очистить ресурсы
+                waveIn.StopRecording();
+                waveIn.Dispose();
+                waveIn = null;
 
+                if (writer != null)
+                {
+                    writer.Close();
+                    writer.Dispose();
+                    writer = null;
+                }
+
+                if (timer != null)
+                {
+                    timer.Stop();
+                    timer.Tick -= Timer_Tick;
+                    timer = null;
+                }
+
+                if (isRecording)
+                {
+                    isRecording = false;
+                    screenCapture.SignalToStop();
+                    screenCapture.WaitForStop();
+                    videoWriter.Close();
+                    videoWriter.Dispose();
+                }
+            }
         }
 
         private void Border_MouseDown(object sender, MouseButtonEventArgs e)
@@ -89,12 +233,12 @@ namespace MitREC
 
         private void InfoButton_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Приложение для записи/захвата экрана было создано с целью импортозамещения для дипломной работы.\nПриложение с понятным интерфейсом и простой настройкой.\nВы можете просто запустить программу и за пару кликов уже начать вести запись!\nНадеюсь, что вам помогла эта программа!", "О приложении",MessageBoxButton.OK);
+            System.Windows.MessageBox.Show("Приложение для записи/захвата экрана было создано с целью импортозамещения для дипломной работы.\nПриложение с понятным интерфейсом и простой настройкой.\nВы можете просто запустить программу и за пару кликов уже начать вести запись!\nНадеюсь, что вам помогла эта программа!", "О приложении",MessageBoxButton.OK);
         }
 
         private void DeveloperButton_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Здравствуй, меня зовут Айдар.\nЯ студент 4 курса с направления Информационных Систем и Технологий.\n\nЕсли ты нашёл какие-то проблемы в программе, то можешь написать мне на почту: novo1233@mail.ru\n\nСпасибо, что пользуешься данным продуктом!","О разработчике", MessageBoxButton.OK);
+            System.Windows.MessageBox.Show("Здравствуй, меня зовут Айдар.\nЯ студент 4 курса с направления Информационных Систем и Технологий.\n\nЕсли ты нашёл какие-то проблемы в программе, то можешь написать мне на почту: novo1233@mail.ru\n\nСпасибо, что пользуешься данным продуктом!","О разработчике", MessageBoxButton.OK);
         }
     }
 }
